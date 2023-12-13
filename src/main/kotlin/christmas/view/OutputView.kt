88cfe9fbbde39bcdd54a7cb3a5b137b12e008593
yeonghyeon <agilecatch@gmail.com>
2023-12-13 22:11:50 +0900

    [Feat] : 날짜 입력 기능
    - 환영문구 출력
    - 날짜 입력받는 기능 완료
    
    관련파일
    - OutputView.kt
    - InputView.kt

--- src/main/kotlin/christmas/view/OutputView.kt
+++ src/main/kotlin/christmas/view/OutputView.kt
@@ -1,4 +1,11 @@
 package christmas.view
 
 class OutputView {
+
+    fun printWelcome() {
+        println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n" +
+                "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)")
+    }
+
+
 }
